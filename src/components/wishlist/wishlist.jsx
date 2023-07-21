864987cdad8dd6b8ac10a3d72f6bd07502cd3011
Ganesh Chaturthi CJ <ganeshchaturthicj@gmail.com>
2023-07-21 18:02:01 +0530

    code updated

--- src/components/wishlist/wishlist.jsx
+++ src/components/wishlist/wishlist.jsx
@@ -6,46 +6,57 @@ import '../nowPlaying/nowPlaying.css';
 import '../wishlist/wishlist.css';
 
 const Wishlist = () => {
-    const { wishList } = useContext(wishListContext);
+  const { wishList, setWishList } = useContext(wishListContext);
 
-    return (
-      <>
-        {wishList.length <= 0 ? (
-          <div className="wishlistMsg">
-            <h1>ADD YOUR FAV HERE</h1>
-          </div>
-        ) : (
-          <>
-            <h1 className="wishlistHeading">YOUR WISHLIST</h1>
+  const clearWishlist = () => {
+    setWishList([]);
+  };
+
+  return (
+    <>
+      {wishList.length <= 0 ? (
+        <div className="wishlistMsg">
+          <h1>ADD YOUR FAV HERE</h1>
+        </div>
+      ) : (
+        <>
+          <h1 className="wishlistHeading">YOUR WISHLIST</h1>
+
+          <button className="wishlistClear" onClick={clearWishlist}>
+            Clear Wishlist{" "}
+          </button>
+
+          <div className="wishListOuterDiv">
             {wishList.map((item) => (
-              <div className="nowPlaying" key={item.movieId}>
-                <div className="nowPlayingMovieCard">
-                  <div className="movie">
-                    <h5>{item.heading}</h5>
-                    <div className="modalImage">
-                      <img
-                        src={item.imageUrl}
-                        width="250px"
-                        height="190px"
-                        alt="ImgFailed"
-                      />
-                    </div>
-                    <div className="movieDescription">
-                      {item.movieDescription}
-                    </div>
-                    <p>{item.language.toUpperCase()}</p>
-                    <p>{item.rating}</p>
-                    <span>{item.movieId}</span>
-                    <span>{item.releaseDate}</span>
+              <div className="nowPlayingg" key={item.movieId}>
+                <div className="movie">
+                  <h5>{item.heading}</h5>
+                  <div className="modalImage">
+                    <img
+                      src={item.imageUrl}
+                      width="250px"
+                      height="190px"
+                      alt="ImgFailed"
+                    />
                   </div>
+                  <div className="movieDescription">
+                    {item.movieDescription}
+                  </div>
+                  <p>{item.language.toUpperCase()}</p>
+                  <p>{item.rating}</p>
+                  <span>{item.movieId}</span>
+                  <span>{item.releaseDate}</span>
                 </div>
               </div>
             ))}
-          </>
-        )}
-      </>
-    );
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Wishlist;
+
+
 
